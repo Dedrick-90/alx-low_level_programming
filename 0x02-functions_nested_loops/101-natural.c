56c0c1d37af6e8681920d591bcaa800247f527d8
Dedrick <tdmapani@icloud.com>
2022-09-14 14:14:11 -0700

    extra

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -6,10 +6,11 @@
  */
 int main(void)
 {
-int n, sum = 0;
+int n = 0; 
+int sum = 0;
 for (n = 0; n < 1024; n++)
 	{
-	if ((n % 3) == 0 || (n % 5) == 0
+	if ((n % 3) == 0 || (n % 5) == 0)
 		sum = sum +n;
 	}
 printf("%d\n", sum);
